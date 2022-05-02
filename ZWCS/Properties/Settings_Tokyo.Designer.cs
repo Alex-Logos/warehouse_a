77f32b9f4cd173dd1c61ecc0e65acf74f607825a
Taku Fujii <takusuke.fujii@logos-consulting.org>
2022-05-02 15:51:16 +0900

    Connection string updated.

--- ZWCS/Properties/Settings_Tokyo.Designer.cs
+++ ZWCS/Properties/Settings_Tokyo.Designer.cs
@@ -49,8 +49,8 @@ namespace Com.ZimVie.Wcs.ZWCS.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Server=10.204.161.9;Port=5432;UserId=postgres;Password=postgres;Database=ZWCS_DEV" +
-            "2;CommandTimeout=300;")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Server=localhost;Port=5432;UserId=postgres;Password=postgres;Database=ZWCS_DEV2;C" +
+            "ommandTimeout=300;")]
         public string CONNECTION_STRING {
             get {
                 return ((string)(this["CONNECTION_STRING"]));
